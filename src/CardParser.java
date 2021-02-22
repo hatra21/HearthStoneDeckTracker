bedbe8a2b9aab7a4e5f081f15a5dfdd4cdce90d8
hatra21 <hunganh3636z@gmail.com>
2021-02-22 12:40:29 -0600

    bubble sort the cards

---
+++ src/CardParser.java
@@ -0,0 +1,68 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.util.ArrayList;
+
+public class CardParser {
+    private String urlString;
+    private ArrayList<HearthStoneCard> theMinions;
+
+    public CardParser(String urlString) {
+        //initial fields
+        this.urlString = urlString;
+        theMinions = new ArrayList<HearthStoneCard>();
+
+        URLReader hearthstoneURLReader = new URLReader(this.urlString);
+        Object obj = JSONValue.parse(hearthstoneURLReader.getTheURLContents());
+
+        if (obj instanceof JSONArray) {
+            //I am only in here if obj IS a JSONArray
+            JSONArray array = (JSONArray) obj;
+
+            for (int i = 0; i < array.size(); i++) {
+                JSONObject cardData = (JSONObject) array.get(i);
+                if (cardData.containsKey("cost") && cardData.containsKey("name")) {
+                    if (cardData.containsKey("type") && cardData.get("type").equals("MINION")) {
+                        //I am only here is this is a minion card!!!
+                        //System.out.println(cardData.keySet().toString());
+                        String name = (String) cardData.get("name");
+                        int cost = Integer.parseInt(cardData.get("cost").toString());
+                        int attack = Integer.parseInt(cardData.get("attack").toString());
+                        int health = Integer.parseInt(cardData.get("health").toString());
+                        HearthStoneCard temp = new HearthStoneCard(name, cost, attack, health);
+                        theMinions.add(temp);
+                    }
+                }
+
+            }
+        }
+    }
+
+    public void showMinions() {
+        for (int i = 0; i < this.theMinions.size(); i++) {
+            this.theMinions.get(i).display();
+        }
+    }
+
+    public void sortLowestCostToHighestCost() {
+        //this methods job is to take our ArrayList of minions and re-arrange it so that
+        //it is in the order of cards with the lowest cost first, and cards with the highest
+        //cost last.
+        //Note: this.theMinions.get(3).getCost() will give you the cost of card #3
+        //Note: this.theMinions.remove(3) will remove the card that used to be at bucket 3
+        //you will need to cobble together your own algorithm for getting this arraylist sorted
+        for (int i = 0; i < this.theMinions.size() - 1; i++) {
+            for (int j = 0; j < this.theMinions.size() - 1 - i; j++) {
+                if (this.theMinions.get(j).getCost() > this.theMinions.get(j + 1).getCost()) {
+                    HearthStoneCard temp = this.theMinions.get(j);
+                    this.theMinions.set(j, theMinions.get(j + 1));
+                    this.theMinions.set(j + 1, temp);
+
+                }
+            }
+
+
+        }
+    }
+}
--- src/Driver.java
+++ src/Driver.java
@@ -1,75 +1,17 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-
 public class Driver {
 
     public static void main(String[] args) {
+        //URLReader hearthstoneURLReader = new URLReader("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
 
-        URLReader hearthstoneURLReader= new URLReader("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
-        //hearthstoneURLReader.display();
-
-        Object obj = JSONValue.parse(hearthstoneURLReader.getTheURLContents());
-        HearthStoneCard[] theMinions = new HearthStoneCard[1924];
-
-
-        if (obj instanceof JSONArray) {
-            JSONArray array = (JSONArray) obj;
-            int count = 0;
-
-            for (int i=0; i< theMinions.length; i++) {
-                JSONObject cardData =(JSONObject) array.get(i);
-                //System.out.println(cardData.get("name"));
-                if (cardData.containsKey("cost") && cardData.containsKey("name")) {
-                    if (cardData.containsKey("type") && cardData.get("type").equals("MINION")) {
-                        //theMinions[i].setName((String) cardData.get("name"));
-                        //theMinions[i] = new HearthStoneCard(cardData.get("name"), cardData.get("cost"), cardData.get("attack"), cardData.get("defense"));
-
-                        String name_converted = cardData.get("name").toString();
-                        int cost_converted = Integer.parseInt(cardData.get("cost").toString());
-                        int att_converted = Integer.parseInt(cardData.get("attack").toString());
-                        int def_converted = Integer.parseInt(cardData.get("health").toString());
-                        HearthStoneCard nextItem = new HearthStoneCard(name_converted,cost_converted,att_converted,def_converted);
-                        theMinions[i] = nextItem;
-
-                        //System.out.println(theMinions[i].getName());
-                        displayCards(theMinions[i].getName(),theMinions[i].getCost(), theMinions[i].getAttack(), theMinions[i].getDefense());
-
-
-
-
-                        //System.out.println(theMinions[i].toString());
-//                        System.out.println();
-
-//
-//                        System.out.println(cardData.get("name"));
-//                        System.out.println(cardData.get("type"));
-//                        System.out.println(cardData.get("cost"));
-//                        System.out.println(cardData.get("attack"));
-//                        System.out.println(cardData.get("defense"));
-//                        System.out.println(cardData.get("health"));
-                        count++;
-
-                    }
-                }
-
-
-            }
-
-            //System.out.println(count);
-
-
-        }
-
-
+        CardParser cp = new CardParser("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
+        cp.sortLowestCostToHighestCost();
+        cp.showMinions();
 
     }
 
-    public static void displayCards(String name, int cost, int att, int hp) {
-        System.out.println("Minion: " + name);
-        System.out.println("Cost: " + cost + " Attack: " + att + " Defense: " + hp);
-        System.out.println();
-    }
+
+
+
 
 
 }
